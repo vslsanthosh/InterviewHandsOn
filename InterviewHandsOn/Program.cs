7a99ac6ecae131f95039681d0d9c4f9feab5efe0
vslsanthosh <vslsanthosh@users.noreply.github.com>
2020-02-09 00:13:41 +0530

    initial commit

--- InterviewHandsOn/Program.cs
+++ InterviewHandsOn/Program.cs
@@ -6,7 +6,27 @@ namespace InterviewHandsOn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
+
+    sealed class myClass1
+    {
+        public int add(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+    }
+
+    class myclass2 : IDisposable
+    {
+        public int add(int no1, int no2)
+        { return no1 + no2; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
